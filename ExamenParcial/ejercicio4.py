ce694ca0ab1ab4529725ad1a3bf489fd27bd85e6
JPGhernandez <pamelagrh23@gmail.com>
2024-03-01 11:49:00 -0600

    Entrega de Parcial

---
+++ ExamenParcial/ejercicio4.py
@@ -0,0 +1,9 @@
+def diccionario(cadena):
+   pila = []
+   dicc = {'titulo', 'autor', 'año de publicacion'}
+
+   for caracter in cadena:
+      if carácter in dicc.values():
+        pila.append(caracter)
+
+print(diccionario)
